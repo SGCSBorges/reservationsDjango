688ed2b13217979d6449fd55ab844a4917b99087
Sérgio Borges <sergio_borges82@hotmail.com>
2024-12-08 22:43:19 +0100

    Fresh start

--- Reservations/catalogue/urls.py
+++ Reservations/catalogue/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 
-from . import views
+from catalogue import views
 
 app_name='catalogue'
 
--- Reservations/catalogue/views/artist.py
+++ Reservations/catalogue/views/artist.py
@@ -6,7 +6,7 @@ from catalogue.models import Artist
 # Create your views here.
 def index(request):
 	artists = Artist.objects.all()
-       title = 'Liste des artistes'
+	title = 'Liste des artistes'
 	
 	return render(request, 'artist/index.html', {
 		'artists':artists,
