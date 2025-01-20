6bb0553923a2275d2a7013440cb3c08576c65e03
Sérgio Borges <sergio_borges82@hotmail.com>
2025-01-20 02:26:10 +0100

    Form update Artist

--- Reservations/catalogue/urls.py
+++ Reservations/catalogue/urls.py
@@ -9,4 +9,5 @@ app_name='catalogue'
 urlpatterns = [
     path('artist/', views.artist.index, name='artist_index'),
     path('artist/<int:artist_id>', views.artist.show, name='artist_show'),
+    path('artist/edit/<int:artist_id>', views.artist.edit, name='artist_edit'),
 ]
--- Reservations/catalogue/views/artist.py
+++ Reservations/catalogue/views/artist.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import Http404
 
 from catalogue.models import Artist
+from catalogue.forms import ArtistForm
 
 # Create your views here.
 def index(request):
@@ -22,6 +23,26 @@ def show(request, artist_id):
 	title = 'Fiche d\'un artiste'
 	
 	return render(request, 'artist/show.html', {
+		'artist':artist,
+		'title':title 
+	})
+
+def edit(request, artist_id):
+	try:
+		artist = Artist.objects.get(id=artist_id)
+	except Artist.DoesNotExist:
+		raise Http404('Artist inexistant');
+		
+	title = 'Modifier un artiste'
+
+	form = ArtistForm(request.POST or None, instance=artist)
+	
+	if request.method == 'POST' and form.is_valid():
+		form.save()
+		return redirect('catalogue:artist_show', artist_id=artist.id)
+	
+	return render(request, 'artist/edit.html', {
+		'form':form,
 		'artist':artist,
 		'title':title 
 	})
