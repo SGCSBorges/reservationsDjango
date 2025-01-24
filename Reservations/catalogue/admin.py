d762b5808db542b24d75b77db1c16cc4b24e9392
Sérgio Borges <sergio_borges82@hotmail.com>
2025-01-24 19:04:23 +0100

    Django Admin configuration

--- Reservations/catalogue/admin.py
+++ Reservations/catalogue/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from .models import (
+    UserMeta,
+    Artist,
+    Locality,
+    Location,
+    Price,
+    Reservation,
+    Show,
+    Type,
+    ArtisteType,
+    Representation,
+    RepresentationReservation,
+    Review
+)
+
 # Register your models here.
+admin.site.register(UserMeta)
+admin.site.register(Artist)
+admin.site.register(Locality)
+admin.site.register(Location)
+admin.site.register(Price)
+admin.site.register(Reservation)
+admin.site.register(Show)
+admin.site.register(Type)
+admin.site.register(ArtisteType)
+admin.site.register(Representation)
+admin.site.register(RepresentationReservation)
+admin.site.register(Review)
