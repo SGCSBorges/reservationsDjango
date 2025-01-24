76df4aa4a9b7f01b091f7c318a9f371b33f420ae
Sérgio Borges <sergio_borges82@hotmail.com>
2025-01-24 23:02:18 +0100

    Frontend authentication – Password recovery

--- Reservations/reservations/urls.py
+++ Reservations/reservations/urls.py
@@ -19,42 +19,38 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
+#Set the django admin site header and title
+# Text to put at the top of the admin index page.
+admin.site.index_title = "Projet Réservations"
+# Text to put in each page's <div id="site-name">.
+admin.site.site_header = "Projet Réservations HEADER"
+# Text to put at the end of each page's <title>.
+admin.site.site_title = "Spectacles"
+
+#Set auth views header and title
+auth_views.PasswordResetView.extra_context = {"site_header": admin.site.site_header,
+                           "site_title": admin.site.site_title,
+                           "index_title": admin.site.index_title,}
+auth_views.PasswordResetDoneView.extra_context = {"site_header": admin.site.site_header,
+                           "site_title": admin.site.site_title,
+                           "index_title": admin.site.index_title,}
+auth_views.PasswordResetConfirmView.extra_context = {"site_header": admin.site.site_header,
+                           "site_title": admin.site.site_title,
+                           "index_title": admin.site.index_title,}
+auth_views.PasswordResetCompleteView.extra_context = {"site_header": admin.site.site_header,
+                           "site_title": admin.site.site_title,
+                           "index_title": admin.site.index_title,}
+auth_views.PasswordChangeView.extra_context = {"site_header": admin.site.site_header,
+                           "site_title": admin.site.site_title,
+                           "index_title": admin.site.index_title,}
+auth_views.PasswordChangeDoneView.extra_context = {"site_header": admin.site.site_header,
+                           "site_title": admin.site.site_title,
+                           "index_title": admin.site.index_title,}
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('compte/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls'),),
     path('catalogue/', include('catalogue.urls')),
-    path(
-        "admin/password_reset/",
-        auth_views.PasswordResetView.as_view(
-            extra_context={"site_header": admin.site.site_header}
-        ),
-        name="admin_password_reset",
-    ),
-    path(
-        "admin/password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(
-            extra_context={"site_header": admin.site.site_header}
-        ),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(
-            extra_context={"site_header": admin.site.site_header}
-        ),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(
-            extra_context={"site_header": admin.site.site_header}
-        ),
-        name="password_reset_complete",
-    ),
     path('admin/', admin.site.urls),
 ]
-
-admin.site.index_title = "Projet Réservations"
-admin.site.index_header = "Projet Réservations HEADER"
-admin.site.site_title = "Spectacles"
 
