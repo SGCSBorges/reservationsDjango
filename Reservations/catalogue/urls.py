a756a79a75539812c6c775d2a8a53ba17c246305
Sérgio Borges <sergio_borges82@hotmail.com>
2025-02-17 00:35:31 +0100

    Add templates & views to locality & price data models

--- Reservations/catalogue/urls.py
+++ Reservations/catalogue/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('artist/edit/<int:artist_id>', views.artist.edit, name='artist_edit'),
     path('artist/create', views.artist.create, name='artist_create'),
     path('artist/delete/<int:artist_id>', views.artist.delete, name='artist_delete'),
+    path('type/', views.type.index, name='type_index'),
+    path('type/<int:type_id>', views.type.show, name='type_show'),
+    path('locality/', views.locality.index, name='locality_index'),
+    path('locality/<int:locality_id>', views.locality.show, name='locality_show'),
+    path('price/', views.price.index, name='price_index'),
+    path('price/<int:price_id>', views.price.show, name='price_show'),
 ]
