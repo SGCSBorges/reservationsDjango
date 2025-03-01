517110020bbeda5514b84dbb502e5462a760e6c9
Sérgio Borges <sergio_borges82@hotmail.com>
2025-03-01 17:30:03 +0100

    fix routes

--- Reservations/catalogue/urls.py
+++ Reservations/catalogue/urls.py
@@ -2,24 +2,29 @@
 
 from django.urls import path
 
-from catalogue import views
+from catalogue.views import artist, type, locality, price, show, representation
 
 app_name='catalogue'
 
 urlpatterns = [
-    path('artist/', views.artist.index, name='artist_index'),
-    path('artist/<int:artist_id>', views.artist.show, name='artist_show'),
-    path('artist/edit/<int:artist_id>', views.artist.edit, name='artist_edit'),
-    path('artist/create', views.artist.create, name='artist_create'),
-    path('artist/delete/<int:artist_id>', views.artist.delete, name='artist_delete'),
-    path('type/', views.type.index, name='type_index'),
-    path('type/<int:type_id>', views.type.show, name='type_show'),
-    path('locality/', views.locality.index, name='locality_index'),
-    path('locality/<int:locality_id>', views.locality.show, name='locality_show'),
-    path('price/', views.price.index, name='price_index'),
-    path('price/<int:price_id>', views.price.show, name='price_show'),
-    path('show/', views.show.index, name='show_index'),
-    path('show/<int:show_id>', views.show.show, name='show_show'),
-    path('representation/', views.representation.index, name='representation_index'),
-    path('representation/<int:representation_id>', views.representation.show, name='representation_show'),
+    path('artist/', artist.index, name='artist_index'),
+    path('artist/<int:artist_id>', artist.show, name='artist_show'),
+    path('artist/edit/<int:artist_id>', artist.edit, name='artist_edit'),
+    path('artist/create', artist.create, name='artist_create'),
+    path('artist/delete/<int:artist_id>', artist.delete, name='artist_delete'),
+
+    path('type/', type.index, name='type_index'),
+    path('type/<int:type_id>', type.show, name='type_show'),
+
+    path('locality/', locality.index, name='locality_index'),
+    path('locality/<int:locality_id>', locality.show, name='locality_show'),
+
+    path('price/', price.index, name='price_index'),
+    path('price/<int:price_id>', price.show, name='price_show'),
+
+    path('show/', show.index, name='show_index'),
+    path('show/<int:show_id>', show.show, name='show_show'),
+    
+    path('representation/', representation.index, name='representation_index'),
+    path('representation/<int:representation_id>', representation.show, name='representation_show'),
 ]
