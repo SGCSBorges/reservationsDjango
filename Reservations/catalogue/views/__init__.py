f53956eb09ce85213c205050ec72d741345c918b
Sérgio Borges <sergio_borges82@hotmail.com>
2025-03-02 18:23:25 +0100

    fix views

--- Reservations/catalogue/views/__init__.py
+++ Reservations/catalogue/views/__init__.py
@@ -1,7 +1,1 @@
-from .artist import *
-from .type import *
-from .locality import *
-from .location import *
-from .price import *
-from .show import *
-from .representation import *
+from . import artist, type, locality, price, show, representation
